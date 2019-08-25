6e80663198903ef26a7e8a687d364e15241d64ef
kusaanko <39370373+kusaanko@users.noreply.github.com>
2019-08-25 16:06:32 +0900

    バージョンの更新

--- src/io/github/kusaanko/MCAddToJar.java
+++ src/io/github/kusaanko/MCAddToJar.java
@@ -17,7 +17,7 @@ import static io.github.kusaanko.Language.*;
 
 public class MCAddToJar extends JFrame {
     public static File mcDir;
-    public static final String version = "1.1.1";
+    public static final String version = "1.1.2";
     public static final String repo = "https://github.com/kusaanko/MCAddToJar/releases";
     public static MCAddToJar frame;
 
