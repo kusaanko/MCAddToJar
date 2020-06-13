0f32818c89e33973aeed767fbd534c5a18edde40
kusaanko <kusa123anko@gmail.com>
2020-06-13 20:09:51 +0900

    Add user download mode

--- src/io/github/kusaanko/modmanager/DownloadingDialog.java
+++ src/io/github/kusaanko/modmanager/DownloadingDialog.java
@@ -29,6 +29,7 @@ public class DownloadingDialog extends JDialog {
 
         this.add(statusLabel);
 
+        this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         this.setSize(400, 200);
         this.setTitle(translate("download"));
         this.setLocationRelativeTo(null);
@@ -45,54 +46,64 @@ public class DownloadingDialog extends JDialog {
                 outputFile = new File(placeFolder, fileName);
 
                 if(!temporary.exists()) {
-                    String downloadURL = link;
-                    URL url = new URL(downloadURL);
-                    URLConnection connection = url.openConnection();
-                    ((HttpURLConnection) connection).setInstanceFollowRedirects(true);
-                    connection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36");
-                    ((HttpURLConnection) connection).setRequestMethod("GET");
-                    connection.connect();
-
-                    if (((HttpURLConnection) connection).getResponseCode() == HttpURLConnection.HTTP_MOVED_TEMP
-                            || ((HttpURLConnection) connection).getResponseCode() == HttpURLConnection.HTTP_MOVED_PERM
-                            || ((HttpURLConnection) connection).getResponseCode() == HttpURLConnection.HTTP_SEE_OTHER) {
-
-                        downloadURL = connection.getHeaderField("Location");
-                        String cookies = connection.getHeaderField("Set-Cookie");
-                        System.out.println("Redirecting to " + downloadURL);
-                        connection = new URL(downloadURL).openConnection();
-
-                        if (cookies != null) {
-                            connection.setRequestProperty("Cookie", cookies);
+                    if(mod.needUserDownload()) {
+                        this.dispose();
+                        WaitingDownloadDialog dialog = new WaitingDownloadDialog(this, mod.getName(), mod.getVersion(), mod.getDownloadPageURL());
+                        while(!dialog.isDownloaded()) {
+                            Thread.sleep(100);
                         }
-                    }
-
-                    if (((HttpURLConnection) connection).getResponseCode() == 200) {
-                        statusLabel.setText(String.format(translate("downloading"), fileName, ""));
-
-                        temporary.getParentFile().mkdirs();
-                        InputStream inputStream = url.openStream();
-                        OutputStream outputStream = new FileOutputStream(temporary);
-
-                        byte[] buff = new byte[4096];
-                        int len;
-                        int downloaded = 0;
-                        while ((len = inputStream.read(buff, 0, buff.length)) != -1) {
-                            outputStream.write(buff, 0, len);
-                            downloaded += len;
-                            statusLabel.setText(String.format(translate("downloading"), fileName, downloaded / 1024 + "KiB / " + connection.getContentLength() / 1024 + "KiB"));
+                        dialog.dispose();
+                        if(dialog.getDownloadedFile() != null && dialog.getDownloadedFile().exists()) {
+                            dialog.getDownloadedFile().renameTo(temporary);
                         }
-                        inputStream.close();
-                        outputStream.close();
-                    } else {
-                        statusLabel.setText(String.format(translate("anerrorhasoccurredhttp"), ((HttpURLConnection) connection).getResponseCode()));
+                    }else {
+                        String downloadURL = link;
+                        URL url = new URL(downloadURL);
+                        URLConnection connection = url.openConnection();
+                        ((HttpURLConnection) connection).setInstanceFollowRedirects(true);
+                        connection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36");
+                        ((HttpURLConnection) connection).setRequestMethod("GET");
+                        connection.connect();
+
+                        if (((HttpURLConnection) connection).getResponseCode() == HttpURLConnection.HTTP_MOVED_TEMP
+                                || ((HttpURLConnection) connection).getResponseCode() == HttpURLConnection.HTTP_MOVED_PERM
+                                || ((HttpURLConnection) connection).getResponseCode() == HttpURLConnection.HTTP_SEE_OTHER) {
+
+                            downloadURL = connection.getHeaderField("Location");
+                            String cookies = connection.getHeaderField("Set-Cookie");
+                            System.out.println("Redirecting to " + downloadURL);
+                            connection = new URL(downloadURL).openConnection();
+
+                            if (cookies != null) {
+                                connection.setRequestProperty("Cookie", cookies);
+                            }
+                        }
+
+                        if (((HttpURLConnection) connection).getResponseCode() == 200) {
+                            statusLabel.setText(String.format(translate("downloading"), fileName, ""));
+
+                            temporary.getParentFile().mkdirs();
+                            InputStream inputStream = url.openStream();
+                            OutputStream outputStream = new FileOutputStream(temporary);
+
+                            byte[] buff = new byte[4096];
+                            int len;
+                            int downloaded = 0;
+                            while ((len = inputStream.read(buff, 0, buff.length)) != -1) {
+                                outputStream.write(buff, 0, len);
+                                downloaded += len;
+                                statusLabel.setText(String.format(translate("downloading"), fileName, downloaded / 1024 + "KiB / " + connection.getContentLength() / 1024 + "KiB"));
+                            }
+                            inputStream.close();
+                            outputStream.close();
+                        } else {
+                            statusLabel.setText(String.format(translate("anerrorhasoccurredhttp"), ((HttpURLConnection) connection).getResponseCode()));
+                        }
                     }
                 }
                 if (mod.getInstallationType() != Mod.INSTALLATION_TYPE.IN_JAR) {
                     Util.copy(temporary, outputFile);
                 } else {
-                    profile.add(temporary.getAbsolutePath());
-                    MCAddToJar.addToJar.update();
                     outputFile = temporary;
                 }
                 event.accept(outputFile);
