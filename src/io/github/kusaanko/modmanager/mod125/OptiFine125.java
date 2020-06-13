7bae92fe488cf5bac32990c92a623e4789ef54ef
kusaanko <39370373+kusaanko@users.noreply.github.com>
2020-06-13 22:37:08 +0900

    Create OptiFine125.java

---
+++ src/io/github/kusaanko/modmanager/mod125/OptiFine125.java
@@ -0,0 +1,60 @@
+package io.github.kusaanko.modmanager.mod125;
+
+import io.github.kusaanko.modmanager.Mod;
+
+public class OptiFine125 extends Mod125 {
+    @Override
+    public String is(String fileName) {
+        return startsWith(fileName, "OptiFine_1.2.5_");
+    }
+
+    @Override
+    public INSTALLATION_TYPE getInstallationType() {
+        return INSTALLATION_TYPE.IN_JAR;
+    }
+
+    @Override
+    public Class<? extends Mod>[] getRequireModsInJar() {
+        return null;
+    }
+
+    @Override
+    public String getDownloadURL() {
+        return null;
+    }
+
+    @Override
+    public String getDownloadPageURL() {
+        return "https://optifine.net/adloadx?f=OptiFine_1.2.5_HD_U_C7.zip";
+    }
+
+    @Override
+    public boolean needUserDownload() {
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return "OptiFine";
+    }
+
+    @Override
+    public String getShortName() {
+        return "OptiFine";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "sp614x";
+    }
+
+    @Override
+    public String getVersion() {
+        return "HD_U_C7";
+    }
+
+    @Override
+    public String getDownloadFileName() {
+        return "OptiFine_1.2.5_HD_U_C7.zip";
+    }
+}
