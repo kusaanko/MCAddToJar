cc0a25c260991f1d3c87a775efc03d9fae574f31
kusaanko <39370373+kusaanko@users.noreply.github.com>
2020-06-20 22:44:59 +0900

    Add patch type processing

--- src/io/github/kusaanko/modmanager/DownloadingDialog.java
+++ src/io/github/kusaanko/modmanager/DownloadingDialog.java
@@ -3,6 +3,7 @@ package io.github.kusaanko.modmanager;
 import io.github.kusaanko.MCAddToJar;
 import io.github.kusaanko.Profile;
 import io.github.kusaanko.Util;
+import io.github.kusaanko.modmanager.mod125.Mod125;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.swing.*;
@@ -101,9 +102,19 @@ public class DownloadingDialog extends JDialog {
                         }
                     }
                 }
-                if (mod.getInstallationType() != Mod.INSTALLATION_TYPE.IN_JAR) {
+                if (mod.getInstallationType() != Mod.INSTALLATION_TYPE.IN_JAR && mod.getType() != Mod.TYPE.PATCH) {
                     Util.copy(temporary, outputFile);
                 } else {
+                    if(mod.getType() == Mod.TYPE.PATCH) {
+                        Mod targetMod = Mod125.mods125.get(mod.getPatchMod());
+                        File outZip;
+                        if(targetMod.getInstallationType() == Mod.INSTALLATION_TYPE.MODS_FOLDER) {
+                            outZip = new File(placeFolder, targetMod.getDownloadFileName());
+                        }else {
+                            throw new IllegalArgumentException("This is not supported type!!(" + targetMod.getInstallationType() + ")");
+                        }
+                        Util.zipCopy(temporary, outZip);
+                    }
                     outputFile = temporary;
                 }
                 event.accept(outputFile);
