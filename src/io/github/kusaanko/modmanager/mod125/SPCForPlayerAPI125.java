7f59ee725ebdbd9fcae025d5616fba3d082d60c8
kusaanko <39370373+kusaanko@users.noreply.github.com>
2020-06-21 16:06:57 +0900

    Add mods

---
+++ src/io/github/kusaanko/modmanager/mod125/SPCForPlayerAPI125.java
@@ -0,0 +1,48 @@
+package io.github.kusaanko.modmanager.mod125;
+
+public class SPCForPlayerAPI125 extends Mod125 {
+    @Override
+    public String is(String fileName) {
+        return startsWith(fileName, "SPC for PlayerAPI Patch 6.0 for SPC ");
+    }
+
+    @Override
+    public String getDownloadURL() {
+        return "http://download1646.mediafire.com/xrsgq55vj3qg/a1kpvbdob0d0ll3/SPC+for+PlayerAPI+Patch+6.0+for+SPC+3.2.2.zip";
+    }
+
+    @Override
+    public String getDownloadPageURL() {
+        return "https://web.archive.org/web/20170816112715/http://www.minecraftforum.net:80/forums/mapping-and-modding/minecraft-mods/1276865-1-5-2-single-player-commands-for-player-api";
+    }
+
+    @Override
+    public INSTALLATION_TYPE getInstallationType() {
+        return INSTALLATION_TYPE.IN_JAR;
+    }
+
+    @Override
+    public String getName() {
+        return "SPC for Player API";
+    }
+
+    @Override
+    public String getShortName() {
+        return "SPC for Player API";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "Divisor";
+    }
+
+    @Override
+    public String getVersion() {
+        return "3.2.2";
+    }
+
+    @Override
+    public String getDownloadFileName() {
+        return "SPC for PlayerAPI Patch 6.0 for SPC 3.2.2.zip";
+    }
+}
---
+++ src/io/github/kusaanko/modmanager/mod125/TurboModelThingy125.java
@@ -0,0 +1,48 @@
+package io.github.kusaanko.modmanager.mod125;
+
+public class TurboModelThingy125 extends Mod125 {
+    @Override
+    public String is(String fileName) {
+        return startsWith(fileName, "turbomodelthingy-v");
+    }
+
+    @Override
+    public String getDownloadURL() {
+        return "http://www.multiverseworks.com/minecraft/turbomodelthingy-v2.3.7-1.2.5.zip";
+    }
+
+    @Override
+    public String getDownloadPageURL() {
+        return "https://www.minecraftforum.net/forums/mapping-and-modding-java-edition/minecraft-mods/1272395-1-2-5-you-know-what-time-it-is-its-garycxjk-time";
+    }
+
+    @Override
+    public INSTALLATION_TYPE getInstallationType() {
+        return INSTALLATION_TYPE.IN_JAR;
+    }
+
+    @Override
+    public String getName() {
+        return "Turbo Model Thingy";
+    }
+
+    @Override
+    public String getShortName() {
+        return "Turbo Model Thingy";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "GaryCXJk";
+    }
+
+    @Override
+    public String getVersion() {
+        return "2.3.7-1.2.5";
+    }
+
+    @Override
+    public String getDownloadFileName() {
+        return "turbomodelthingy-v2.3.7-1.2.5.zip";
+    }
+}
