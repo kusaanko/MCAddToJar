4a17c4b4aeaba5859077b86a5484d6087808f39e
kusaanko <kusa123anko@gmail.com>
2020-06-21 21:22:12 +0900

    Add mods

---
+++ src/io/github/kusaanko/modmanager/mod125/RailCraft_CraftGuideFix125.java
@@ -0,0 +1,60 @@
+package io.github.kusaanko.modmanager.mod125;
+
+import io.github.kusaanko.modmanager.Mod;
+
+public class RailCraft_CraftGuideFix125 extends Mod125 {
+    @Override
+    public String is(String fileName) {
+        return null;
+    }
+
+    @Override
+    public String getDownloadURL() {
+        return "";
+    }
+
+    @Override
+    public String getDownloadPageURL() {
+        return "";
+    }
+
+    @Override
+    public TYPE getType() {
+        return TYPE.PATCH;
+    }
+
+    @Override
+    public PATCH_TYPE getPatchType() {
+        return PATCH_TYPE.DELETE_FILES;
+    }
+
+    @Override
+    public Class<? extends Mod> getPatchMod() {
+        return RailCraft125.class;
+    }
+
+    @Override
+    public String getName() {
+        return "RailCraft CraftGuide Fix";
+    }
+
+    @Override
+    public String getShortName() {
+        return "RailCraft CraftGuide Fix";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "Kusaanko";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0";
+    }
+
+    @Override
+    public String getDownloadFileName() {
+        return "";
+    }
+}
