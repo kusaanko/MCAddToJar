81b671bf4bd20851c01254f30f9b0d874941f985
kusaanko <kusa123anko@gmail.com>
2020-06-21 15:57:28 +0900

    Fix author name

--- src/io/github/kusaanko/modmanager/mod125/AdditionalPipes125.java
+++ src/io/github/kusaanko/modmanager/mod125/AdditionalPipes125.java
@@ -40,7 +40,7 @@ public class AdditionalPipes125 extends Mod125 {
 
     @Override
     public String getAuthor() {
-        return "Zombie Killer";
+        return "DaStormbringer";
     }
 
     @Override
