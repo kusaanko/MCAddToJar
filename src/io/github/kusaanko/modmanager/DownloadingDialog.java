868891fb57893ab143dcdf3b006badb25f13a7cd
kusaanko <kusa123anko@gmail.com>
2020-06-21 19:40:01 +0900

    Add referer

--- src/io/github/kusaanko/modmanager/DownloadingDialog.java
+++ src/io/github/kusaanko/modmanager/DownloadingDialog.java
@@ -66,6 +66,7 @@ public class DownloadingDialog extends JDialog {
                         URL url = new URL(downloadURL);
                         URLConnection connection = url.openConnection();
                         ((HttpURLConnection) connection).setInstanceFollowRedirects(true);
+                        connection.addRequestProperty("Referer", mod.getReferer());
                         connection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36");
                         ((HttpURLConnection) connection).setRequestMethod("GET");
                         connection.connect();
