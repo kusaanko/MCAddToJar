6e9ef0c9001ef944eb355e810e868694a8927465
kusaanko <kusa123anko@gmail.com>
2020-07-04 16:52:09 +0900

    Change to display all versions for update history, supports Japanese

--- src/io/github/kusaanko/NewVersionOldMCPatcher.java
+++ src/io/github/kusaanko/NewVersionOldMCPatcher.java
@@ -1,5 +1,8 @@
 package io.github.kusaanko;
 
+import com.google.gson.Gson;
+import com.google.gson.internal.LinkedTreeMap;
+
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
@@ -7,10 +10,12 @@ import java.awt.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import static io.github.kusaanko.Language.language;
 import static io.github.kusaanko.Language.translate;
 
 public class NewVersionOldMCPatcher extends JDialog {
@@ -20,24 +25,33 @@ public class NewVersionOldMCPatcher extends JDialog {
         this.setModal(true);
         this.setTitle(translate("newversion"));
         JPanel main = new JPanel(new BorderLayout());
-        JPanel panel = new JPanel(new GridLayout(0, 1));
+        JPanel panel = new JPanel(new BorderLayout());
         JPanel textpanel = new JPanel(new GridLayout(0, 1));
         JPanel buttonpanel = new JPanel(new GridLayout(1, 0));
         textpanel.add(label(translate("oldmcpatcherupdated")));
         textpanel.add(label(translate("preversion")+":<font style=\"color: red;font-weight: bold;\">"+prever));
         textpanel.add(label(translate("currentversion")+":<font style=\"color: green;font-weight: bold;\">"+newver));
         textpanel.add(label("<font style=\"font-size: 20px;font-weight: bold;\">"+translate("changelog")));
-        panel.add(textpanel);
+        panel.add(textpanel, BorderLayout.NORTH);
         {
-            String body = Util.executeGet("https://api.github.com/repos/kusaanko/OldMCPatcher/releases");
-            Matcher matcher = Pattern.compile("\"body\"[^:]:[^\"]\"([^\"]*)\"").matcher(body);
-            if(matcher.find()) {
-                body = matcher.group(1);
-                JScrollPane pane = new JScrollPane(label("<font style=\"font-weight: bold;\">"+body.replace("\r", "").replace("\n", "<br>").replace("\\r", "").replace("\\n", "<br>")));
-                pane.setPreferredSize(new Dimension(100, 100));
-                pane.setSize(200, 100);
-                panel.add(pane);
+            String link = "https://api.github.com/repos/kusaanko/OldMCPatcher/releases";
+            if(language.equals("ja_JP")) {
+                link = "https://raw.githubusercontent.com/kusaanko/OldMCPatcher/master/changelog_jp.json";
             }
+            String body = Util.executeGet(link);
+
+            java.util.List<Object> jsonList = new Gson().fromJson(body, List.class);
+            String changelog = "";
+            for(Object obj : jsonList) {
+                LinkedTreeMap<String, Object> map = Util.toMap(obj);
+                changelog += "<font style=\"font-weight: bold;font-size: 20px;\">" + map.get("name") + "</font>\n";
+                changelog += map.get("body") + "\n\n";
+            }
+            JScrollPane pane = new JScrollPane(label(changelog.replace("\r", "").replace("\n", "<br>").replace("\\r", "").replace("\\n", "<br>")));
+            pane.setPreferredSize(new Dimension(100, 100));
+            pane.setSize(200, 100);
+            pane.getVerticalScrollBar().setUnitIncrement(10);
+            panel.add(pane, BorderLayout.CENTER);
         }
         JButton button = new JButton("<html><font style=\"font-size: 14px;\">OK");
         button.addActionListener(e -> {
@@ -78,14 +92,14 @@ public class NewVersionOldMCPatcher extends JDialog {
         buttonpanel.add(button);
         main.add(buttonpanel, BorderLayout.SOUTH);
         this.add(main);
-        this.setSize(500, 400);
+        this.setSize(600, 500);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     }
 
     private JLabel label(String name) {
-        JLabel label = new JLabel("<html><font style=\"font-size: 14px;\">"+name);
+        JLabel label = new JLabel("<html><font style=\"font-size: 14px;font-family: sanserif\">"+name);
         Border margin = new EmptyBorder(5,2,5,10);
         label.setBorder(margin);
         return label;
